0876eb39ce5101683cdd3ba1bf477b33312544f5
Selvakumar Subramaniam <selvakumar.subramaniam@hotmail.com>
2021-09-24 11:21:09 +0100

    The Preview 4.1 Compiled version

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using ContosoUniversity.Extensions;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace ContosoUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.IndexPageRequested();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
